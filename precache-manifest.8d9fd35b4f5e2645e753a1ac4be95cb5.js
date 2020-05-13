e7d58092db1f91632fe9f517020382615118b49a
Xian Li <xnslx89@gmail.com>
2020-05-13 09:49:51 -0500

    Updates

---
+++ precache-manifest.8d9fd35b4f5e2645e753a1ac4be95cb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d4a742722388f76978f2f0330d5d54",
+    "url": "/React-Counter/index.html"
+  },
+  {
+    "revision": "5430cf4680f767a03e17",
+    "url": "/React-Counter/static/css/main.5e9e753a.chunk.css"
+  },
+  {
+    "revision": "03859ead6e63aec0c3a6",
+    "url": "/React-Counter/static/js/2.0dd42102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Counter/static/js/2.0dd42102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5430cf4680f767a03e17",
+    "url": "/React-Counter/static/js/main.199a8656.chunk.js"
+  },
+  {
+    "revision": "ac16e48f9d637ba36e7b",
+    "url": "/React-Counter/static/js/runtime-main.b684df62.js"
+  }
+]);
